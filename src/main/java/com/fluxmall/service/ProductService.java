800685cb77f6c561ab6913f54c9ea507e1c5bec4
Jungho Kim <jeonghogim318@gmail.com>
2026-01-09 15:56:24 +0900

    set: Service 클래스 추가

---
+++ src/main/java/com/fluxmall/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.fluxmall.service;
+
+
+import com.fluxmall.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductMapper productMapper;
+
+    public Product findById(Long id) {
+        return null;
+    }
+
+    public List<Product> findAllWithPaging(/* Paging DTO */) {
+        return null;
+    }
+
+    public List<Product> searchByKeyword(/* Search DTO */) {
+        return null;
+    }
+
+    public void register(Product product) {
+    }
+
+    public void update(Product product) {
+    }
+
+    public void updateStock(Long productId, int quantity) {
+    }
+
+    public void softDelete(Long id) {
+    }
+}
