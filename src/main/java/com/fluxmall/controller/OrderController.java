5d6de0829eb937fbb6098b4ce12d1deba82671f0
Jungho Kim <jeonghogim318@gmail.com>
2026-01-09 16:01:07 +0900

    set: 컨트롤러 클래스 추가

---
+++ src/main/java/com/fluxmall/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.fluxmall.controller;
+
+
+import com.fluxmall.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+}
